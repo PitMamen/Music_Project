ce22b15c9fb3d251fbad9a18bc0179f0a63d7d89
zhujiang <zhujiang.shishi@foxmail.com>
2016-06-22 16:05:56 +0800

    add Music home page

--- app/src/main/java/com/android/app/MusicHomeActivity.java
+++ app/src/main/java/com/android/app/MusicHomeActivity.java
@@ -1,8 +1,10 @@
 package com.android.app;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ListView;
 
+import com.dlighttech.music.adapter.HomeAdapter;
 import com.dlighttech.music.model.ContentItem;
 
 import java.util.ArrayList;
@@ -12,20 +14,51 @@ public class MusicHomeActivity extends BaseActivity {
 
     private ListView mListView;
     private List<ContentItem> mData;
+    private HomeAdapter mAdapter;
 
     @Override
     public void onCreateView() {
         setContentView(R.layout.music_home_page_layout);
+
         mListView = (ListView) findViewById(R.id.home_list_view);
+        mAdapter = new HomeAdapter(this, mData);
+        mListView.setAdapter(mAdapter);
 
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                ArrayList<ContentItem> items=FileUtils.getFileList(MusicHomeActivity.this,FileUtils.Music);
+//                for (ContentItem item:items) {
+//                }
+                // 获取所有歌曲id
+                long[] ids= MusicUtils.getAllSongs(MusicHomeActivity.this);
+                for (int i=0;i<ids.length;i++){
+                    // 获取歌曲的专辑图片
+                    Bitmap b =MusicUtils.getArtwork(MusicHomeActivity.this,ids[i],0);
 
+                    Log.d("TAG",ids[i]+"====="+b);
+
+                }
+                mAdapter.updateItem();
+            }
+        },2000);
     }
 
     @Override
     public void onCreateData() {
         mData = new ArrayList<ContentItem>();
-//        ContentItem itemDir = new ContentItem()
-
+        ContentItem itemDir = new ContentItem(R.drawable.seek_thumb
+                , R.drawable.left, "文件夹");
+        ContentItem itemPlayList = new ContentItem(R.drawable.playlist_tile
+                , R.drawable.left, "播放列表");
+        ContentItem itemUpdate = new ContentItem(R.drawable.app_music
+                , R.drawable.left, "最近更新");
+        ContentItem itemPlay = new ContentItem(R.drawable.stat_notify_musicplayer
+                , R.drawable.left, "最近播放");
+        mData.add(itemDir);
+        mData.add(itemPlayList);
+        mData.add(itemUpdate);
+        mData.add(itemPlay);
     }
 
     @Override
---
+++ app/src/main/java/com/dlighttech/music/model/MusicInfo.java
@@ -0,0 +1,14 @@
+package com.dlighttech.music.model;
+
+/**
+ * Created by zhujiang on 16-6-22.
+ */
+public class MusicInfo {
+    // 歌手
+    // 歌曲名
+    // 歌曲总时间
+    // 歌曲当前时间
+    // 歌曲当前状态
+
+
+}
