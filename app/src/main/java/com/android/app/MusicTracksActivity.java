51c64c556590b541a95264f2cac3d0e96024cc22
PXK <541931879@qq.com>
2016-06-23 15:57:59 +0800

    add music trancks

---
+++ app/src/main/java/com/android/app/MusicTracksActivity.java
@@ -0,0 +1,21 @@
+package com.android.app;
+
+/**
+ * Created by pengxinkai001 on 2016/6/23.
+ */
+public class MusicTracksActivity extends  BaseActivity {
+    @Override
+    public void onCreateView() {
+
+    }
+
+    @Override
+    public void onInitView() {
+
+    }
+
+    @Override
+    public void onCreateData() {
+
+    }
+}
